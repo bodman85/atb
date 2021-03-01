2d546578980a59576fb92db28d08bc49d6c7bdf4
Bogdan Zgurskyi <v-bzghur@microsoft.com>
2021-03-01 18:06:26 +0200

    Fetching futures symbols

---
+++ script.js
@@ -0,0 +1,69 @@
+const FUTURES_URL = "https://dapi.binance.com/dapi/v1/exchangeInfo";
+
+window.onload = async function () {
+    initFuturesDropDowns();
+}
+
+function initFuturesDropDowns() {
+    requestFuturesSymbols(fillFuturesDropdowns);
+}
+
+function fillFuturesDropdowns(response) {
+    let futuresDropdown1 = document.getElementById("futures-dropdown-1");
+    let futuresDropdown2 = document.getElementById("futures-dropdown-2");
+    removeAllChildNodes(futuresDropdown1);
+    removeAllChildNodes(futuresDropdown2);
+    for (let s of response.symbols) {
+        futuresDropdown1.appendChild(createOption(s.symbol, s.symbol));
+        futuresDropdown2.appendChild(createOption(s.symbol, s.symbol));
+    }
+    sortSelectOptions(futuresDropdown1);
+    sortSelectOptions(futuresDropdown2);
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function createOption(label, value) {
+    var option = document.createElement("option");
+    option.setAttribute("value", value);
+    option.innerHTML = label;
+    return option;
+}
+
+function requestFuturesSymbols(callback) {
+    fireGetRequestTo(FUTURES_URL, callback);
+}
+
+async function fireGetRequestTo(url, callback) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(JSON.parse(xhttp.responseText));
+        }
+    };
+    xhttp.open("GET", url, true);
+    //xhttp.setRequestHeader('Authorization', `Bearer ${tokenWithExpiration.token}`);
+    xhttp.send();
+}
+
+function sortSelectOptions(selElem) {
+    var tmpAry = new Array();
+    for (var i = 0; i < selElem.options.length; i++) {
+        tmpAry[i] = new Array();
+        tmpAry[i][0] = selElem.options[i].text;
+        tmpAry[i][1] = selElem.options[i].value;
+    }
+    tmpAry.sort();
+    while (selElem.options.length > 0) {
+        selElem.options[0] = null;
+    }
+    for (var i = 0; i < tmpAry.length; i++) {
+        var op = new Option(tmpAry[i][0], tmpAry[i][1]);
+        selElem.options[i] = op;
+    }
+    return;
+}
