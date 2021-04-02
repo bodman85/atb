2f9c37544a1ce3bef50f6571915ad5332d1aaf32
Bogdan Zgurskyi <v-bzghur@microsoft.com>
2021-04-02 14:01:28 +0300

    Updates

--- best_prices.js
+++ best_prices.js
@@ -13,20 +13,54 @@
 /*!****************************!*\
   !*** ./src/best_prices.js ***!
   \****************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("window.onload = async function () {\r\n    const urlParams = new URLSearchParams(window.location.search);\r\n    const myParam = urlParams.get('pussy');\r\n    console.log('Hi ' + myParam);\r\n}\n\n//# sourceURL=webpack://atb/./src/best_prices.js?");
+eval("var dataManager = __webpack_require__(/*! ./data-manager */ \"./src/data-manager.js\");\r\n\r\nwindow.onload = async function () {\r\n    const urlParams = new URLSearchParams(window.location.search);\r\n\r\n    let leadingInstrumentSymbol = urlParams.get('leadingInstrument');\r\n    let ledInstrumentSymbol = urlParams.get('ledInstrument');\r\n\r\n    document.getElementById('bpLeadingInstrument1').value = leadingInstrumentSymbol;\r\n    document.getElementById('bpLeadingInstrument2').value = leadingInstrumentSymbol;\r\n\r\n    document.getElementById('bpLedInstrument1').value = ledInstrumentSymbol;\r\n    document.getElementById('bpLedInstrument2').value = ledInstrumentSymbol;\r\n\r\n    setInterval(pollBestPricesAndRecomputeDeltas, 1000, leadingInstrumentSymbol, ledInstrumentSymbol);\r\n}\r\n\r\nfunction pollBestPricesAndRecomputeDeltas(leadingInstrumentSymbol, ledInstrumentSymbol) {\r\n\r\n    dataManager.requestBestPrices(leadingInstrumentSymbol, (response) => {\r\n        let leadingPrice1 = document.getElementById(`bpLeadingPrice1`);\r\n        let ledPrice1 = document.getElementById(`bpLedPrice1`);\r\n        let deltaPcnt1 = document.getElementById(`bpDeltaPcnt1`);\r\n        let deltaUsd1 = document.getElementById(`bpDeltaUsd1`);\r\n        if (response[0]) {\r\n            leadingPrice1.value = response[0].bidPrice;\r\n            ledPrice1.value = response[0].askPrice;\r\n            deltaPcnt1.value = parseFloat(((ledPrice1.value / leadingPrice1.value - 1) * 100).toFixed(4));\r\n            deltaUsd1.value = (parseFloat(ledPrice1.value - leadingPrice1.value).toFixed(4));\r\n        }\r\n    });\r\n\r\n    dataManager.requestBestPrices(ledInstrumentSymbol, (response) => {\r\n        let leadingPrice2 = document.getElementById(`bpLeadingPrice2`);\r\n        let ledPrice2 = document.getElementById(`bpLedPrice2`);\r\n        let deltaPcnt2 = document.getElementById(`bpDeltaPcnt2`);\r\n        let deltaUsd2 = document.getElementById(`bpDeltaUsd2`);\r\n        if (response[0]) {\r\n            leadingPrice2.value = response[0].askPrice;\r\n            ledPrice2.value = response[0].bidPrice;\r\n            deltaPcnt2.value = parseFloat(((leadingPrice2.value / ledPrice2.value - 1) * 100).toFixed(4));\r\n            deltaUsd2.value = (parseFloat(leadingPrice2.value - ledPrice2.value).toFixed(4));\r\n        }\r\n    });\r\n}\n\n//# sourceURL=webpack://atb/./src/best_prices.js?");
+
+/***/ }),
+
+/***/ "./src/data-manager.js":
+/*!*****************************!*\
+  !*** ./src/data-manager.js ***!
+  \*****************************/
+/***/ ((module) => {
+
+eval("const SERVER_URL = \"https://dapi.binance.com/dapi/v1/\"\r\n\r\nconst ALL_SYMBOLS = \"exchangeInfo\";\r\nconst SYMBOL_PRICE = \"ticker/price\";\r\nconst BEST_PRICES = \"ticker/bookTicker\";\r\n\r\nlet cachedSymbols = [];\r\n\r\nasync function getAllSymbols() {\r\n    if (cachedSymbols.length === 0) {\r\n        let response = await fireGetRequestTo(ALL_SYMBOLS);\r\n        cachedSymbols = response.symbols;\r\n    } \r\n    return cachedSymbols;\r\n}\r\n\r\nasync function fireGetRequestTo(path) {\r\n    let url = SERVER_URL + path;\r\n    let requestHeaders = new Headers();\r\n    requestHeaders.append('Content-Type', 'application/json');\r\n    //if (token = await tokenManager.getValidAuthToken()) {\r\n    //    requestHeaders.append('Authorization', `Bearer ${token}`);\r\n    //}\r\n    const params = {\r\n        method: 'GET',\r\n        headers: requestHeaders,\r\n    };\r\n    const response = await fetch(url, params);\r\n    const json = await response.json();\r\n    return json;\r\n}\r\n\r\nfunction requestPrice(symbol, callback) {\r\n    let path = `${SYMBOL_PRICE}?symbol=${symbol}`;\r\n    fireGetRequestWithCallback(path, callback);\r\n}\r\n\r\nfunction requestBestPrices(symbol, callback) {\r\n    let path = `${BEST_PRICES}?symbol=${symbol}`;\r\n    fireGetRequestWithCallback(path, callback);\r\n}\r\n\r\nasync function fireGetRequestWithCallback(path, callback) {\r\n    let url = SERVER_URL + path;\r\n    let xhttp = new XMLHttpRequest();\r\n    xhttp.onreadystatechange = function () {\r\n        if (this.readyState == 4 && this.status == 200) {\r\n            callback(JSON.parse(xhttp.responseText));\r\n        }\r\n    };\r\n    xhttp.open(\"GET\", url, true);\r\n    //xhttp.setRequestHeader('Authorization', `Bearer ${tokenWithExpiration.token}`);\r\n    xhttp.send();\r\n}\r\n\r\nmodule.exports = {\r\n    getAllSymbols: getAllSymbols,\r\n    requestPrice: requestPrice,\r\n    requestBestPrices: requestBestPrices\r\n}\r\n\r\n\r\n\n\n//# sourceURL=webpack://atb/./src/data-manager.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/best_prices.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/best_prices.js");
 /******/ 	
 /******/ })()
 ;
