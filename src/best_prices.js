fe3d797d20a1de0c3e19fdfea5c04a4233df2f56
Bogdan Zgurskyi <v-bzghur@microsoft.com>
2021-04-08 18:25:50 +0300

    recalculating target deltas oninput

--- src/best_prices.js
+++ src/best_prices.js
@@ -5,6 +5,13 @@ var uiUtils = require("./ui-utils");
 window.onload = async function () {
     const urlParams = new URLSearchParams(window.location.search);
 
+    document.getElementById('bpTargetDeltaPcnt1').addEventListener('input', fixTargetSpread);
+    document.getElementById('bpTargetDeltaUsd1').addEventListener('input', fixTargetSpread);
+
+    document.getElementById('bpTargetDeltaPcnt2').addEventListener('input', fixTargetSpread);
+    document.getElementById('bpTargetDeltaUsd2').addEventListener('input', fixTargetSpread);
+    
+
     let leadingInstrumentSymbol = urlParams.get('leadingInstrument');
     let ledInstrumentSymbol = urlParams.get('ledInstrument');
 
@@ -25,6 +32,23 @@ window.onload = async function () {
     setInterval(pollBestPricesAndRecomputeDeltas, 1000, leadingInstrumentSymbol, ledInstrumentSymbol);
 }
 
+let targetSpreadFixedInPcnt1 = true;
+let targetSpreadFixedInPcnt2 = true;
+function fixTargetSpread() {
+    switch (this.id) {
+        case 'bpTargetDeltaPcnt1':
+            targetSpreadFixedInPcnt1 = true;
+            break;
+        case 'bpTargetDeltaUsd1':
+            targetSpreadFixedInPcnt1 = false;
+        case 'bpTargetDeltaPcnt2':
+            targetSpreadFixedInPcnt2 = true;
+            break;
+        case 'bpTargetDeltaUsd2':
+            targetSpreadFixedInPcnt2 = false;
+    }
+}
+
 function pollBestPricesAndRecomputeDeltas(leadingInstrumentSymbol, ledInstrumentSymbol) {
     dataManager.requestBestPrices((response) => {
         let filteredResponse = response.filter(item => [leadingInstrumentSymbol, ledInstrumentSymbol].includes(item.symbol));
@@ -35,10 +59,35 @@ function pollBestPricesAndRecomputeDeltas(leadingInstrumentSymbol, ledInstrument
         let ledBidPrice = filteredResponse.filter(item => item.symbol === ledInstrumentSymbol).map(obj => obj.bidPrice);
         let ledOfferPrice = filteredResponse.filter(item => item.symbol === ledInstrumentSymbol).map(obj => obj.askPrice);
 
-        document.getElementById(`bpDeltaPcnt1`).value = parseFloat(((ledOfferPrice / leadingBidPrice - 1) * 100).toFixed(4))+' %';
-        document.getElementById(`bpDeltaUsd1`).value = (parseFloat(ledOfferPrice - leadingBidPrice).toFixed(4))+' $';
+        let deltaPcnt1 = parseFloat(((ledOfferPrice / leadingBidPrice - 1) * 100).toFixed(4));
+        let deltaUsd1 = parseFloat(ledOfferPrice - leadingBidPrice).toFixed(4);
 
-        document.getElementById(`bpDeltaPcnt2`).value = parseFloat(((ledBidPrice / leadingOfferPrice - 1) * 100).toFixed(4))+' %';
-        document.getElementById(`bpDeltaUsd2`).value = (parseFloat(ledBidPrice - leadingOfferPrice).toFixed(4))+' $';
+        let deltaPcnt2 = parseFloat(((ledBidPrice / leadingOfferPrice - 1) * 100).toFixed(4));
+        let deltaUsd2 = parseFloat(ledBidPrice - leadingOfferPrice).toFixed(4);
+
+        document.getElementById(`bpDeltaPcnt1`).value = deltaPcnt1 + ' %';
+        document.getElementById(`bpDeltaUsd1`).value = deltaUsd1 + ' $';
+
+        document.getElementById(`bpDeltaPcnt2`).value = deltaPcnt2 +' %';
+        document.getElementById(`bpDeltaUsd2`).value = deltaUsd2 + ' $';
+
+        let targetDeltaPcnt1 = document.getElementById(`bpTargetDeltaPcnt1`).value;
+        let targetDeltaUsd1 = document.getElementById(`bpTargetDeltaUsd1`).value;
+
+        let targetDeltaPcnt2 = document.getElementById(`bpTargetDeltaPcnt2`).value;
+        let targetDeltaUsd2 = document.getElementById(`bpTargetDeltaUsd2`).value;
+
+        //recompute target deltas
+        if (targetSpreadFixedInPcnt1 && targetDeltaPcnt1) {
+            document.getElementById(`bpTargetDeltaUsd1`).value = (deltaUsd1 * targetDeltaPcnt1 / deltaPcnt1).toFixed(4);
+        } else if (!targetSpreadFixedInPcnt1 && targetDeltaUsd1) {
+            document.getElementById(`bpTargetDeltaPcnt1`).value = (deltaPcnt1 * targetDeltaUsd1 / deltaUsd1).toFixed(4);
+        }
+
+        if (targetSpreadFixedInPcnt2 && targetDeltaPcnt2) {
+            document.getElementById(`bpTargetDeltaUsd2`).value = (deltaUsd2 * targetDeltaPcnt2 / deltaPcnt2).toFixed(4);
+        } else if (!targetSpreadFixedInPcnt2 && targetDeltaUsd2) {
+            document.getElementById(`bpTargetDeltaPcnt2`).value = (deltaPcnt2 * targetDeltaUsd2 / deltaUsd2).toFixed(4);
+        }
     });
 }
