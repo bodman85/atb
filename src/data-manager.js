b9d6b572e7ef2678d2ca4e27f80f9187394934bc
Bogdan Zgurskyi <v-bzghur@microsoft.com>
2021-06-03 17:52:42 +0300

    Removing keys from repo

--- src/data-manager.js
+++ src/data-manager.js
@@ -100,8 +100,6 @@ function requestBestPrices(callback) {
 }
 
 function executeOrder(order, callback) {
-    //test apiKey = '0d59086bc89d630eb5d6df7d174ad4eed4bc35f3207332dccd6717ad843dea13';
-    //test secretKey = '68037734469c00cde71dece5527908e3350d4b03583275458fb5ae7eae28c118';
     let buyQueryString = `symbol=${order.buy}&side=BUY&type=MARKET&quantity=1&timeStamp=${Date.now()}&recvWindow=60000`;
     let sellQueryString = `symbol=${order.sell}&side=SELL&type=MARKET&quantity=1&timeStamp=${Date.now()}&recvWindow=60000`;
     buyQueryString += sign(buyQueryString);
