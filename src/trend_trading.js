8eb581893a185d37c2456859c79b13f78159ad27
Bogdan Zgurskyi <v-bzghur@microsoft.com>
2021-07-30 12:16:15 +0300

    bugfix related to previousMin and previousMax

--- src/trend_trading.js
+++ src/trend_trading.js
@@ -89,14 +89,14 @@ window.onload = async function () {
                     placeOrder('BUY', 'MARKET');
                     let takeProfitPrice = addPcntDelta(currentPrice, TAKE_PROFIT_SWING_IN_CHANNEL_PCNT);
                     placeOrder('SELL', 'LIMIT', takeProfitPrice);
-                    let stopLossPrice = parseFloat(currentPrice - parseFloat((previousMin + previousMax) / 2)).toFixed(2);
+                    let stopLossPrice = parseFloat(currentPrice - (previousMax - previousMin) / 2).toFixed(2);
                     placeOrder('SELL', 'STOP', stopLossPrice);
                 } else if (previousMax > 0 && currentPrice > previousMax) {
                     console.log(`Price reached top of Channel`);
                     placeOrder('SELL', 'MARKET');
                     let takeProfitPrice = addPcntDelta(currentPrice, -TAKE_PROFIT_SWING_IN_CHANNEL_PCNT);
                     placeOrder('BUY', 'LIMIT', takeProfitPrice);
-                    let stopLossPrice = parseFloat(currentPrice + parseFloat((previousMin + previousMax) / 2)).toFixed(2);
+                    let stopLossPrice = parseFloat(currentPrice + (previousMax - previousMin) / 2).toFixed(2);
                     placeOrder('BUY', 'STOP', stopLossPrice);
                 }
             }
