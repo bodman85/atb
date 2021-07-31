77d334f47f812b8938311121460c1a851878e4bd
Bogdan Zgurskyi <v-bzghur@microsoft.com>
2021-07-31 20:41:09 +0300

    oscillatorMaxFast and oscillatorMinFast should be > 0

--- src/trend_trading.js
+++ src/trend_trading.js
@@ -10,7 +10,7 @@ const FORECAST_DELTA_EDGE_VALUE = 0.1;
 
 const TREND_DELTA_PCNT = 0.05;
 const TAKE_PROFIT_FOLLOW_TREND_PCNT = 0.25;
-const TAKE_PROFIT_OSCILLATOR_PCNT = 0.3;
+const TAKE_PROFIT_OSCILLATOR_PCNT = 0.25;
 const STOP_LOSS_PRICE_PCNT = 0.25;
 const LIMIT_ORDER_FEE_PCNT = 0.01;
 const LIMIT_ORDER_TRIGGER_PRICE_PCNT = 0.05;
@@ -155,12 +155,12 @@ function isTrendDesc() {
 }
 
 function isMarketOverbought() {
-    return oscillatorSlidingAverageFast > 0 && oscillatorSlidingAverageSlow > 0 && currentPrice > oscillatorMaxFast
+    return oscillatorSlidingAverageFast > 0 && oscillatorSlidingAverageSlow > 0 && oscillatorMaxFast > 0 && currentPrice > oscillatorMaxFast
         && getPcntGrowth(oscillatorSlidingAverageSlow, oscillatorSlidingAverageFast) >= TAKE_PROFIT_OSCILLATOR_PCNT
 }
 
 function isMarketOversold() {
-    return oscillatorSlidingAverageFast > 0 && oscillatorSlidingAverageSlow > 0 && currentPrice < oscillatorMinFast
+    return oscillatorSlidingAverageFast > 0 && oscillatorSlidingAverageSlow > 0 && oscillatorMinFast >0 && currentPrice < oscillatorMinFast
         && getPcntGrowth(oscillatorSlidingAverageSlow, oscillatorSlidingAverageFast) <= -TAKE_PROFIT_OSCILLATOR_PCNT
 }
 
